7cb8952d8041b1862b47cad786cf6a01b56188d8
kiiita <kitter2011@gmail.com>
2014-09-16 20:21:03 +0900

    3-3が完了した所

---
+++ SwifferApp/SweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SweetTableViewCell.swift
+//  SwifferApp
+//
+//  Created by kiiita on 2014/09/16.
+//  Copyright (c) 2014年 Training. All rights reserved.
+//
+
+import UIKit
+
+class SweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
