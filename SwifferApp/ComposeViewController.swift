843ecc5c9f961df244d02263f7296a84aaa0e0eb
kiiita <kitter2011@gmail.com>
2014-09-16 20:36:02 +0900

    3-5まで完了した所

--- SwifferApp/ComposeViewController.swift
+++ SwifferApp/ComposeViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    @IBOutlet weak var sweetTextView: UITextView! = UITextView()
+    @IBOutlet weak var charRemaining: UILabel! = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +23,16 @@ class ComposeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func sendSweet(sender: AnyObject) {
+        
+        
+        var sweet:PFObject = PFObject(className: "Sweets") // Tweetを保存するための変数を作る
+        sweet["content"] = sweetTextView.text // Tweet内容を取得
+        sweet["sweeter"] = PFUser.currentUser() // 現在のユーザーの取得
+        
+        sweet.saveInBackground() // ツイートの情報を保存する
+        self.navigationController?.popToRootViewControllerAnimated(true) // 保存が終わったらトップ画面へ自動的に戻る
+    }
 
     /*
     // MARK: - Navigation
